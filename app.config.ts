60364d91ee42dfa8c47b75209e25a74bfe1e9f28
Mish-al7 <mishal.07.911@gmail.com>
2026-05-16 13:44:56 +0530

    use vinxi createApp in config

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,11 @@
+import { createApp } from "vinxi";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-export default {
+export default createApp({
   vite: {
     plugins: [
       tanstackRouter(),
@@ -14,4 +15,4 @@ export default {
       tailwindcss(),
     ],
   },
-};
+});
